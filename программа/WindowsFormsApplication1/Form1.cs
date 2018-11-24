b45b8ac3eed6ced2d90b987605c40798a2b3ad74
KostyaUstyancev <kostik-ustyantsev@mail.ru>
2018-11-24 13:42:05 +0500

    V0.1

---
+++ программа/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string way = "";
+        
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.ShowNewFolderButton = false;
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                way = FBD.SelectedPath;
+            }
+            DirectoryInfo dir = new DirectoryInfo(way);
+            textBox2.Text += "\n\r" + way + "\r\n";
+            FileInfo[] Files = dir.GetFiles("*.xlsx", SearchOption.TopDirectoryOnly);
+           
+            foreach (FileInfo fail in Files)
+            {
+
+                textBox2.Text += "\n\r" + fail.Name + "\r\n";
+
+               
+            }
+            foreach (FileInfo fail in Files)
+            {
+                Excel.Application excel = new Excel.Application();
+                Workbook workbookb = excel.Workbooks.Open(way + @"\" + fail.Name);
+                Worksheet excelSheet = workbookb.ActiveSheet;
+                workbookb.Close();
+
+
+
+
+
+            }
+        }
+
+        
+    }
+}
