9a1b3f286159c4eaa08751860c288836550c4080
Студенческая Учетная запись факультета информатики <student_kash@study.rsvpu.ru>
2018-12-15 08:36:15 +0500

    1

--- программа/UnitTest1/UnitTest1.cs
+++ программа/UnitTest1/UnitTest1.cs
@@ -7,101 +7,100 @@ namespace UnitTest1
     [TestClass]
     public class UnitTest1
     {
-        //    [TestMethod]
-        //    public void TestMethodDATA1()
-        //    {
-        //        string str = "";
-        //        bool ogedaem = false;
-        //        Form1 t = new Form1();
-        //        bool rezult = t.Date(str);
-        //        Assert.AreEqual(ogedaem,rezult);
+        [TestMethod]
+        public void TestMethodDATA1()
+        {
+            string str = "";
+            bool ogedaem = false;
+            Form1 t = new Form1();
+            bool rezult = t.Date(str);
+            Assert.AreEqual(ogedaem, rezult);
 
-        //    }
+        }
 
-        //    [TestMethod]
-        //    public void TestMethodDATA2()
-        //    {
-        //        string str = "12.12.2017";
-        //        bool ogedaem = true;
-        //        Form1 t = new Form1();
-        //        bool rezult = t.Date(str);
-        //        Assert.AreEqual(ogedaem, rezult);
+        [TestMethod]
+        public void TestMethodDATA2()
+        {
+            string str = "12.12.2017";
+            bool ogedaem = true;
+            Form1 t = new Form1();
+            bool rezult = t.Date(str);
+            Assert.AreEqual(ogedaem, rezult);
 
-        //    }
-
-
-        //    [TestMethod]
-        //    public void TestMethodDATA3()
-        //    {
-        //        string str = "12.13.2017";
-        //        bool ogedaem = false;
-        //        Form1 t = new Form1();
-        //        bool rezult = t.Date(str);
-        //        Assert.AreEqual(ogedaem, rezult);
-
-        //    }
-
-        //    [TestMethod]
-        //    public void TestMethodDUBL1()
-        //    {
-        //        string str = "12.13";
-        //        bool ogedaem = false;
-        //        Form1 t = new Form1();
-        //        bool rezult = t.Dubl(str);
-        //        Assert.AreEqual(ogedaem, rezult);
-        //    }
-
-        //    [TestMethod]
-        //    public void TestMethodDUBL2()
-        //    {
-        //        string str = "12,13";
-        //        bool ogedaem = true;
-        //        Form1 t = new Form1();
-        //        bool rezult = t.Dubl(str);
-        //        Assert.AreEqual(ogedaem, rezult);
-        //    }
+        }
 
 
-        //    [TestMethod]
-        //    public void TestMethodDUBL3()
-        //    {
-        //        string str = "";
-        //        bool ogedaem = false;
-        //        Form1 t = new Form1();
-        //        bool rezult = t.Dubl(str);
-        //        Assert.AreEqual(ogedaem, rezult);
-        //    }
+        [TestMethod]
+        public void TestMethodDATA3()
+        {
+            string str = "12.13.2017";
+            bool ogedaem = false;
+            Form1 t = new Form1();
+            bool rezult = t.Date(str);
+            Assert.AreEqual(ogedaem, rezult);
+
+        }
+
+        [TestMethod]
+        public void TestMethodDUBL1()
+        {
+            string str = "12.13";
+            bool ogedaem = false;
+            Form1 t = new Form1();
+            bool rezult = t.Dubl(str);
+            Assert.AreEqual(ogedaem, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethodDUBL2()
+        {
+            string str = "12,13";
+            bool ogedaem = true;
+            Form1 t = new Form1();
+            bool rezult = t.Dubl(str);
+            Assert.AreEqual(ogedaem, rezult);
+        }
 
 
-        //    [TestMethod]
-        //    public void TestMethodCislo1()
-        //    {
-        //        string str = "1";
-        //        bool ogedaem = true;
-        //        Form1 t = new Form1();
-        //        bool rezult = t.chislo(str);
-        //        Assert.AreEqual(ogedaem, rezult);
-        //    }
+        [TestMethod]
+        public void TestMethodDUBL3()
+        {
+            string str = "";
+            bool ogedaem = false;
+            Form1 t = new Form1();
+            bool rezult = t.Dubl(str);
+            Assert.AreEqual(ogedaem, rezult);
+        }
 
-        //    [TestMethod]
-        //    public void TestMethodCislo2()
-        //    {
-        //        string str = "1.4";
-        //        bool ogedaem = false;
-        //        Form1 t = new Form1();
-        //        bool rezult = t.chislo(str);
-        //        Assert.AreEqual(ogedaem, rezult);
-        //    }
 
-        //    [TestMethod]
-        //    public void TestMethodCislo3()
-        //    {
-        //        string str = "строка";
-        //        bool ogedaem = false;
-        //        Form1 t = new Form1();
-        //        bool rezult = t.chislo(str);
-        //        Assert.AreEqual(ogedaem, rezult);
-        //    }
+        [TestMethod]
+        public void TestMethodCislo1()
+        {
+            string str = "1";
+            bool ogedaem = true;
+            Form1 t = new Form1();
+            bool rezult = t.chislo(str);
+            Assert.AreEqual(ogedaem, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethodCislo2()
+        {
+            string str = "1.4";
+            bool ogedaem = false;
+            Form1 t = new Form1();
+            bool rezult = t.chislo(str);
+            Assert.AreEqual(ogedaem, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethodCislo3()
+        {
+            string str = "строка";
+            bool ogedaem = false;
+            Form1 t = new Form1();
+            bool rezult = t.chislo(str);
+            Assert.AreEqual(ogedaem, rezult);
+        }
     }
-
 }
